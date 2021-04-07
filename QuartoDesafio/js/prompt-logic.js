586017156f69dd824c7571288f95dae3f89892bc
BuddhiLW <pedrogbranquinho@gmail.com>
2021-04-07 09:54:23 -0300

    Implementation of a purely functional prompt. All we needed: a prompt and a filter functions.
    The initialize function is a composition of the two.

--- QuartoDesafio/js/prompt-logic.js
+++ QuartoDesafio/js/prompt-logic.js
@@ -1,1 +1,10 @@
+var initGame = () => filterCards(howMany());
 
+var howMany = () => prompt("With how many cards you wish to play with?");
+
+var filterCards = number =>
+    (number % 2 === 0 && (number > 4 && number < 14))
+    ? createCards(number)
+    : initGame();
+
+var createCards = x => x;
