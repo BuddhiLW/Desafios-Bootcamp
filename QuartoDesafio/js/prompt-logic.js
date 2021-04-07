c9e2f5c35e21171c6145c119381fa52c7dc97a18
BuddhiLW <pedrogbranquinho@gmail.com>
2021-04-07 19:40:06 -0300

    Implementation of populate cards function

--- QuartoDesafio/js/prompt-logic.js
+++ QuartoDesafio/js/prompt-logic.js
@@ -7,26 +7,19 @@ var filterCards = number =>
     ? createCards(number)
     : initGame();
 
-let li = document.createElement('li');
+var querier = (e) => document.querySelector(e);
+var populateWith = (e) => (li) => e.appendChild(li.cloneNode(true));
+
 var ul = querier('ul');
+let li = document.createElement('li');
 
-var querier = e => document.querySelector(e);
-var populateWith = e => li => e.appendChild(li);
-
-var populateLi = number => function(number){
-    var values = ul;
-    var accumulate = function (li){
-        if (values.length < number){
-            populateWith(values)(li);
-            return values;
-        }else{
-            return values;
-        }
-        return accumulate;
-    }; 
+var populate = (number) => {
+  console.log(number);
+  for (var i = 0; i < number; i++) {
+    populateWith(ul)(li);
+  }
 };
-// for (var i = 0; i < number; i++) {
-//     return populateWith(ul)(li);
-// }
 
-var createCards = number => populate(number);
+var createCards = (number) => populate(number);
+
+initGame();
